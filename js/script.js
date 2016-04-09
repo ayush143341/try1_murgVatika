1ef8d1f36393d4758fb54a395dff9f19306803a1
Ayushman Kumar Priyadarshan <ayush143341@gmail.com>
2016-04-09 16:22:56 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*jquery to get the alert message starts*/
+
+$(
+   function()
+   {
+      $('#alertMe').click(function(e){e.preventDefault();$('#successAlert').slideDown();});
+   }
+);
+
+/*jquery to get the alert message starts*/
